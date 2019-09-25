e42d35c830ee442ee3b4630001fe12d7a990e915
qxu <antwork@126.com>
2019-09-25 10:03:42 +0800

    remove platform limit

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SQLCipher",
-    platforms: [
-        .macOS(.v10_12), .iOS(.v9)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
